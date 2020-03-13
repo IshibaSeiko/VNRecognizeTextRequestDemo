1d629cd8bb864f24c99ba23cbf9b709612fcd96d
IshibaSeiko <seiko.ishiba@st-ventures.jp>
2020-03-13 15:45:29 +0900

    initial commit

---
+++ VNRecognizeTextRequestDemo/VNRecognizeTextRequestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VNRecognizeTextRequestDemo
+//
+//  Created by STV-M025 on 2020/03/13.
+//  Copyright © 2020 STV-M025. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
